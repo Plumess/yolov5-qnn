e7b0ab0872178166a369cf1426eb9b116f1a2412
“Plume” <agcl1021@gmail.com>
2024-08-08 20:35:07 +0800

    Update train.py to use torch.amp.autocast('cuda', amp)

--- yolov5/train.py
+++ yolov5/train.py
@@ -321,7 +321,8 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
                     imgs = nn.functional.interpolate(imgs, size=ns, mode='bilinear', align_corners=False)
 
             # Forward
-            with torch.cuda.amp.autocast(amp):
+            # with torch.cuda.amp.autocast(amp):
+            with torch.amp.autocast('cuda', amp):
                 pred = model(imgs)  # forward
                 loss, loss_items = compute_loss(pred, targets.to(device))  # loss scaled by batch_size
                 if RANK != -1:
